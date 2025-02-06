30a3208d141ee078d97745a964229a3b5c0742b9
eugene-kotyashov <eugene.kotyashov@gmail.com>
2025-02-06 17:16:31 +0300

    added user and pass fields

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Int_Input.H>
+#include <FL/Fl_Secret_Input.H>  // For password field
 
 #include "camera_device.hpp"
 #include "ui_utils.hpp"
@@ -22,10 +23,18 @@ void connect_cb(Fl_Widget* widget, void* camera_device_ptr) {
     Fl_Window* window = widget->window();
     Fl_Input* ip = (Fl_Input*)window->child(0);
     Fl_Int_Input* port = (Fl_Int_Input*)window->child(1);
+    Fl_Input* username = (Fl_Input*)window->child(2);
+    Fl_Secret_Input* password = (Fl_Secret_Input*)window->child(3);
+
     const char* ip_value = ip->value();
     int port_value = atoi(port->value());
+    const char* username_value = username->value();
+    const char* password_value = password->value();
+
     CameraDevice* camera_device = (CameraDevice*)camera_device_ptr;
-    camera_device->connect(ip_value, port_value, "admin", "123456");
+    camera_device->connect(
+        ip_value, port_value,
+         username_value, password_value);
     if (camera_device->loggedUserId < 0) {
         ui::MessageDialog::showError(
             "Failed to connect to camera " +
@@ -57,7 +66,7 @@ int main(int argc, char *argv[]) {
     }
     
     
-    Fl_Window window (300, 250, "FLTK Window");
+    Fl_Window window (300, 300, "Camera Connection");
     window.begin();
 
     // Add IP input field
@@ -67,22 +76,28 @@ int main(int argc, char *argv[]) {
     // Add Port input field
     Fl_Int_Input port_input(240, 20, 50, 25, "Port:");
     port_input.value("8080");  // Default port value
+
+    // Add Username field
+    Fl_Input username_input(70, 55, 220, 25, "User:");
+    username_input.value("admin");  // Default username
+
+    // Add Password field
+    Fl_Secret_Input password_input(70, 90, 220, 25, "Pass:");
+    password_input.value("123456");  // Default password
     
     // Add Connect and Disconnect buttons
-    Fl_Button connect_btn(70, 60, 80, 30, "Connect");
+    Fl_Button connect_btn(70, 130, 80, 30, "Connect");
     connect_btn.callback(connect_cb, &camera_device);
 
-    Fl_Button disconnect_btn(160, 60, 80, 30, "Disconnect");
+    Fl_Button disconnect_btn(160, 130, 80, 30, "Disconnect");
     disconnect_btn.callback(disconnect_cb, &camera_device);
 
     // Move OK button lower
-    Fl_Button button(110, 200, 80, 30, "OK");
+    Fl_Button button(110, 250, 80, 30, "OK");
     button.callback(button_cb, &camera_device);
 
     window.end();
     window.show(argc, argv);
-
-    
 
     // Enter the FLTK event loop
     return Fl::run();
